55603d2004585b08084bd1d2a5b98edb9ed73ac2
HelpSlipFrank <calufa01@gmail.com>
2017-06-27 16:48:24 -0400

    Updated to include a check if the number is prime.

--- divisors.py
+++ divisors.py
@@ -1,8 +1,11 @@
 """
 http://www.practicepython.org/exercise/2014/02/26/04-divisors.html
+http://www.practicepython.org/exercise/2014/04/16/11-check-primality-functions.html
 Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
 (If you don't know what a divisor is, it is a number that divides evenly into another number.
 For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+2017-06-27  Added a check if the number is prime
 """
 
 def divisors():
@@ -18,6 +21,11 @@ def divisors():
             break
 
     div = [n for n in range(1, val+1) if val % n == 0]
-    print div
+
+    if len(div) == 2:
+        print("'%d' is a prime number!" % val)
+    else:
+        print("'%d' has %d divisors, which are listed below:" % (val,len(div)))
+        print div
 
 divisors()
